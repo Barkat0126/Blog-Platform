18af1a5cfd3f2ff055cc6f53931468036cc323d9
Barkat0126 <barkat0126@gmail.com>
2024-02-19 20:19:02 +0530

    auth

--- src/store/authSlice.js
+++ src/store/authSlice.js
@@ -1,8 +1,8 @@
-import {createSlice} from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    status: true,
-    userData : null
+    status : false,
+    userData: null
 }
 
 const authSlice = createSlice({
@@ -10,17 +10,16 @@ const authSlice = createSlice({
     initialState,
     reducers: {
         login: (state, action) => {
-            state.status = true
-            state.userData = action.payload.userData
+            state.status = true;
+            state.userData = action.payload.userData;
         },
-
         logout: (state) => {
-            state.status = false
-            state.userData = null
+            state.status = false;
+            state.userData = null;
         }
-    }
+     }
 })
 
-export const {login, logout} = authSlice.actions
+export const {login, logout} = authSlice.actions;
 
 export default authSlice.reducer;
